ced43844ccee67e82255ef7bf29d49df3244a177
Mohan <mohan.pitchikala@sumtotalsystems.com>
2019-11-16 20:49:35 +0530

    paris of numbers sum

---
+++ paris_of_numbers_sum_<_x.js
@@ -0,0 +1,26 @@
+// Given an array of numbers in sorted order, count the paris of numbers whose sum is less than X
+
+let defaultArr = [2, 4, 6, 8, 9]
+const x = 9
+
+let counter = 0
+
+function repeater (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let sum = arr[i] + arr[arr.length - 1]
+    if (sum < x) {
+      counter = counter + 1
+    }
+
+    if (sum > x) {
+      let currentArray = arr.slice(0, arr.length - 1)
+      console.log(currentArray)
+      repeater(currentArray)
+      break
+    }
+  }
+}
+
+repeater(defaultArr)
+
+console.log(counter, 'counter')
